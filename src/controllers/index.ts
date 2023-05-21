043cc612af21924151761674fb43799c3d67ffb3
LuisRojas <capoterojas123@gmail.com>
2023-05-20 21:30:51 -0500

    Creacion de controladores

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,3 +3,6 @@ export * from './login-user.controller';
 export * from './user-login.controller';
 export * from './role-user.controller';
 export * from './user-role.controller';
+export * from './user.controller';
+export * from './role.controller';
+export * from './login.controller';
